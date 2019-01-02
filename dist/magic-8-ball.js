7d30b9b0baefe8f822829b4f6feebcec421a2b47
Chris <hello@christopherporter.co.uk>
2019-01-02 12:50:17 +0000

    Tidying up and tweaking values

--- dist/magic-8-ball.js
+++ dist/magic-8-ball.js
@@ -37,19 +37,19 @@ function magicEightBall() {
   // Output random response
   response.innerHTML = responses[randomiser];
 
-  // Add class to the container
+  // (Optional) Add class to the container once populated
   if (response.classList)
     response.classList.add(className);
   else
     response.className += ' ' + className;
 
-  // (Optional) Randomise the values for 3D transform
-  var xDeg = Math.floor(Math.random() * 20);
-  var yDeg = Math.floor(Math.random() * 20);
-  var zDeg = Math.floor(Math.random() * 20);
+  // (Optional) Create randomised values for CSS 3D transform animation
+  var xDeg = Math.floor(Math.random() * 40);
+  var yDeg = Math.floor(Math.random() * 40);
+  var zDeg = Math.floor(Math.random() * 40);
   var deg = Math.floor(Math.random() * 80);
 
-  // Create style tag to populate
+  // Create style tag to populate with CSS animation
   var style = (function() {
     var style = document.createElement("style");
     style.appendChild(document.createTextNode(""));
@@ -57,8 +57,8 @@ function magicEightBall() {
     return style;
   })();
 
-  // Populate style tag with dynamic property
-  style.sheet.insertRule('@keyframes responseAnimation { 0% { opacity: 0; transform-origin: 50% 50%; transform: rotate3d(' + xDeg + ', -' + yDeg + ',' + zDeg + ',' + deg + 'deg) scale3d(.8, .8, .8); } 50% { opacity: .4; } 100% { opacity: 1; } }', 0);
+  // Populate created style tag with CSS animation property
+  style.sheet.insertRule('@keyframes transformAndScale { 0% { transform-origin: 50% 50%; transform: rotate3d(' + xDeg + ', -' + yDeg + ',' + zDeg + ',' + deg + 'deg) scale3d(.8, .8, .8); } 100% { } }', 0);
 
 }
 
